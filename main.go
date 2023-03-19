85826a1d9981415187383787bdcb8e018f53eaa1
namikaze-dev <usmansadik96@gmail.com>
2023-03-19 12:48:43 +0100

    Add play with compound strategy option

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 	case strategyMixed:
 		playStrategyMixed(a, b)
 	default:
-		fmt.Println("compound")
+		playCompoundStrategy(a, b)
 	}
 }
 
@@ -79,6 +79,27 @@ func playStrategyMixed(a, b string) {
 
 		fmt.Printf("Holding at %v vs Holding at %v: wins: %v/%v (%v.0%%), losses: %v/%v (%v%%)\n",
 			a, i + 1, winsA[i], games, percentA, winsB[i], games, percentB,
+		)
+	}
+}
+
+func playCompoundStrategy(a, b string) {
+	minA, maxA := parseRange(a)
+	minB, maxB := parseRange(b)
+
+	winsA, winsB := CompoundStrategy(minA, maxA, minB, maxB, games)
+	playedGames := (maxB - minB) * games
+
+	for i := range winsA {
+		if fmt.Sprint(i + 1) == a {
+			continue
+		}
+
+		percentA := int(float32(winsA[i]) / float32(playedGames) * 100)
+		percentB := int(float32(winsB[i]) / float32(playedGames) * 100)
+
+		fmt.Printf("Result: Wins, losses staying at k = %v: %v/%v (%v%%), %v/%v (%v%%)\n",
+			i + 1, winsA[i], playedGames, percentA, winsB[i], playedGames, percentB,
 		)
 	}
 }
