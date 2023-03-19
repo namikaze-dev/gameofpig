39c1a47563078d909b9f064fda6d45a8297abf28
namikaze-dev <usmansadik96@gmail.com>
2023-03-19 09:35:47 +0100

    Implement fixed strategy game play

--- main.go
+++ main.go
@@ -32,3 +32,39 @@ func (p *Player) HoldAfterN(n int) int {
 	p.total += score
 	return score
 }
+
+func (p *Player) score() int {
+	return p.total
+}
+
+func FixedStrategy(firstN, secondN, games int) (int, int) {
+	firstWins, secondWins := 0, 0
+
+	for i := 0; i < games; i++ {
+		first := NewPlayer(true)
+		second := NewPlayer(false)
+		turn := true
+
+		for {
+			if turn {
+				first.HoldAfterN(firstN)
+			} else {
+				second.HoldAfterN(secondN)
+			}
+			// switch turns
+			turn = !turn
+
+			if first.score() >= 100 {
+				firstWins++
+				break
+			}
+
+			if second.score() >= 100 {
+				secondWins++
+				break
+			}
+		}
+	}
+
+	return firstWins, secondWins
+}
