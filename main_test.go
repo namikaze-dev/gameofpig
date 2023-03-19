4081d18c76ccd3ae1b309081d45f2af86e429930
namikaze-dev <usmansadik96@gmail.com>
2023-03-19 09:48:21 +0100

    Add test for fixed strategy game play

--- main_test.go
+++ main_test.go
@@ -26,3 +26,27 @@ func TestPlayer(t *testing.T) {
 		t.Errorf("got %v want >= %v", got, want)
 	}
 }
+
+func TestFixedStrategyGamePlay(t *testing.T) {
+	firstN, secondN, games := 10, 15, 10
+	firstWins, secondWins := main.FixedStrategy(firstN, secondN, games)
+
+	got := firstWins + secondWins
+	if got != games {
+		t.Errorf("total games: got %v want %v", got, games)
+	}
+
+	// higher chance of rand results 1000 games 
+	firstN, secondN, games = 10, 15, 1000
+	firstWins, secondWins = main.FixedStrategy(firstN, secondN, games)
+
+	got = firstWins + secondWins
+	if got != games {
+		t.Errorf("total games: got %v want %v", got, games)
+	}
+
+	// NOTE: there's a small chance that wins would be equal, expect occasional failure
+	if firstWins == secondWins {
+		t.Errorf("wins should not be equal, got %v and %v", firstWins, secondWins)
+	}
+}
