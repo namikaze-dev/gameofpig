f26b2521bf3d01783b58479dec5d31cc936f33d5
namikaze-dev <usmansadik96@gmail.com>
2023-03-19 08:37:03 +0100

    Implement player struct with HoldAfterN func

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Player struct {
+	player bool
+	total  int
+	rand   *rand.Rand
+}
+
+func NewPlayer(p bool) Player {
+	return Player{
+		player: p,
+		rand:   rand.New(rand.NewSource(time.Now().UnixMicro())),
+	}
+}
+
+// Rolls die untill current score >= N, returns current score.
+func (p *Player) HoldAfterN(n int) int {
+	var score int
+	for score <= n {
+		got := p.rand.Intn(7)
+		// player scores nothing
+		if got == 1 {
+			return 0
+		}
+		score += got
+	}
+	p.total += score
+	return score
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,28 @@
 package main_test
+
+import (
+	"testing"
+
+	"github.com/namikaze-dev/gameofpig"
+)
+
+func TestPlayer(t *testing.T) {
+	// true for player 1, false for player two 
+	player := main.NewPlayer(true)
+
+	// hold after 10
+	want := 10
+	got := player.HoldAfterN(want)
+
+	if got < want && got != 0 {
+		t.Errorf("got %v want >= %v", got, want)
+	}
+
+	// hold after 99, increase chance of scoring 0
+	want = 99
+	got = player.HoldAfterN(want)
+
+	if got < want && got != 0 {
+		t.Errorf("got %v want >= %v", got, want)
+	}
+}
